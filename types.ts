05cca15c51af185e2e79e4de514d2c4ed9f6a9fc
fabricioluna <fabricioluna@gmail.com>
2026-02-01 10:25:00 -0300

    Update types.ts

--- types.ts
+++ types.ts
@@ -2,10 +2,15 @@ export type Position = 'Zagueiro' | 'Meia' | 'Atacante';
 
 export interface Player {
   id: string;
+  code: string; // Código de 3 dígitos (ex: 042)
   name: string;
   position: Position;
-  level: number;
-  isFixedInTeam1?: boolean; // Nova propriedade para os campeões
+  level: number; // 1 a 10
+  redCards: number;
+  goals: number;
+  // Propriedades usadas apenas durante o sorteio
+  isFixedInTeam1?: boolean;
+  isPresent?: boolean; // Para marcar presença na lista do sorteio
 }
 
 export interface Team {
